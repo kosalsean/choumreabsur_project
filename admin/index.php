eafb9c29b876f0bf7fc65cc15f9bff058ff37b16
Kosal <sean.kosal@outlook.com>
2019-09-14 17:04:56 +0700

    admin design

--- admin/index.php
+++ admin/index.php
@@ -5,10 +5,44 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <script src="js/jquery-3.2.1.min.js"></script>
+    <link rel="stylesheet" href="style/fontawesome-free-5.3.1-web/css/all.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Hanuman|Roboto&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="style/style.css">
     <title>Document</title>
 </head>
 <body>
+<div class="header">
+    <div class="left-header">
+        1
+    </div>
+    <div class="right-header">
+        2
+    </div>
+</div>
+<div class="main-content">
+    <div class="left-menu">
+        <div class="menu">
+            <ul>
+                <li>
+                 <i class="fas fa-align-center"></i> &nbsp;Menu
+                </li>
+                <li>
+                    Content
+                </li>
+                <li>
+                    Advertise
+                </li>
+                <li>
+                    Setting
+                </li>
+            </ul>
+        </div>
+    </div>
+    <div class="content">
+        2
+    </div>
+</div>
+
     
 
 
